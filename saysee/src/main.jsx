c86c5e6d59fb8fb3980145a86c275cdac3bf66f7
mrbornmann <mr.bornmann@gmail.com>
2026-06-02 23:05:50 -0700

    Delete saysee directory

--- saysee/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SaySee from './SaySee.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SaySee />
-  </React.StrictMode>
-)
