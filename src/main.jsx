6e21ea1d3bbf0d5a459323a0fa28877ff6b2a064
mrbornmann <mr.bornmann@gmail.com>
2026-06-02 23:29:03 -0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SaySee from './SaySee.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SaySee />
+  </React.StrictMode>
+)
